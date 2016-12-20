252131eb5cf812a1c5233c5a4852ecc18f5b7138
Paul Legler <paullegler@tests-MacBook-Pro-4.local>
2016-12-19 21:43:30 -0800

    initiliaze

---
+++ board.py
@@ -0,0 +1,10 @@
+class Board():
+    def __init__(self):
+        self.resouce_positions = self.generate_initial_positions()
+        self.settlements = []
+        self.roads = []
+        self.cities = []
+
+    """ Initialize the board positions """
+    def generate_initial_positions(self):
+        return None
---
+++ resourceCards.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class ResourceCards():
+    def __init__(self):
+        self.deck = self.initiliaze_resource_cards() ## python list
+
+    def initiliaze_resource_cards(self):
+        return None
